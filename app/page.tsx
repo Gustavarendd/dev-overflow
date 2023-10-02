270c0fd6ad6569cba32efe8ed281e9c829cb49f5
Gustavarendd <111903425+Gustavarendd@users.noreply.github.com>
2023-10-02 20:26:05 +0200

    theme, global.css and tailwind config

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 import Image from 'next/image';
 
 export default function Home() {
-  return <main></main>;
+  return (
+    <div>
+      <h1 className="h1-bold">Here we go!!!</h1>
+      <h2 className="h2-bold">Here we go!!!</h2>
+      <h3 className="h3-bold">Here we go!!!</h3>
+    </div>
+  );
 }
