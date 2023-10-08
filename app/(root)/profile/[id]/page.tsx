a2bd869d5208dde70da8aecba310640f7aaf2c10
Gustavarendd <111903425+Gustavarendd@users.noreply.github.com>
2023-10-08 15:09:38 +0200

    typo

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -73,7 +73,7 @@ const Page = async ({ params, searchParams }: URLProps) => {
         </div>
       </div>
       <Stats
-        totalQUestions={userInfo.totalQuestions}
+        totalQuestions={userInfo.totalQuestions}
         totalAnswers={userInfo.totalAnswers}
       />
       <div className="mt-10 flex gap-10">
